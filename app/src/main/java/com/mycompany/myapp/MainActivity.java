8bdb1b781bcba8da6b97af3556165f346ea5977b
gunaseelan <gunabca@gmail.com>
2018-09-26 19:24:33 +0530

    test commit

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+    {//hi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
